c37cd0e48cd98e0b98a56db180e641bd9a7eeb16
Anoop Wilson <anoopwilson33@yahoo.com>
2024-02-19 12:16:23 +0530

    .

--- 009-javascript-basics1/index.js
+++ 009-javascript-basics1/index.js
@@ -1,6 +1,6 @@
 // sum of two numbers//
 
-function myFunction(a,b){
+/*function myFunction(a,b){
 let sum= a+b
 console.log(sum)
 }
@@ -11,7 +11,7 @@ myFunction(1000,999)
 function hiFunction() {
   alert('Welcome')
   
-}
+}*/
 
 //90 or above is A+ | 80 or above is A | 70 or above is B+ | 60 or above is B | 50 or above is C+ | Below 50 is failed//
 
